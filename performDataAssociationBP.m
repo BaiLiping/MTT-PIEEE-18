6d5096011f485f10eb38834769e9480b1d3c5a7b
Liping Bai <blp_engineer@outlook.com>
2025-01-05 13:30:24 +0100

    update

--- performDataAssociationBP.m
+++ performDataAssociationBP.m
@@ -15,11 +15,17 @@ om = ones(1,numMeasurements);
 on = ones(1,numObjects);
 messages2 = ones(numMeasurements,numObjects);
 
+% missed detection does not depend on the measurement
+% therefore no need to update it
+% in the normalization step, the missed detection is added separately
+
 for iteration = 1:numIterations
     messages2Old = messages2;
-    
+    % the multiplication in the denominator of equation 30
     product1 = messages2 .* inputLegacy(2:end,:);
+    % demoninator of equation 30
     sum1 = inputLegacy(1,:) + sum(product1,1);
+
     messages1 = inputLegacy(2:end,:) ./ (sum1(om,:) - product1);
     sum2 = inputNew + sum(messages1,2);
     messages2 = 1 ./ (sum2(:,on) - messages1);
